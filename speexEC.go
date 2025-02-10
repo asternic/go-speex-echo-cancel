37ffe4c6badf8592553a67c497e4a20884359e22
DavidJones10 <102177843+DavidJones10@users.noreply.github.com>
2025-02-10 12:23:43 -0500

    Fix aec to use Echo Cancel module instead of preprocessing module

--- speexEC.go
+++ speexEC.go
@@ -3,31 +3,30 @@ package speexEC
 /*
 #cgo LDFLAGS: -lspeexdsp
 #include <speex/speex_echo.h>
-#include <speex/speex_preprocess.h>
 #include <stdlib.h>
+
+typedef struct {
+	SpeexEchoState *echo_state;
+} EchoCanceller;
+
+EchoCanceller* speex_aec_init(int frame_size, int filter_length, int sample_rate){
+	EchoCanceller* ec = (EchoCanceller*)malloc(sizeof(EchoCanceller));
+	ec->echo_state = speex_echo_state_init(frames_size, filter_length);
+	speex_echo_ctl(ec->echo_state, SPEEX_ECHO_SET_SAMPLING_RATE, &sample_rate);
+	return ec;
+}
+
+void speex_aec_process(EchoCanceller *ec, short *mic, short *ref, short *out){
+	speex_echo_cancel(ec->echo_state, mic, ref, out)
+}
 
 SpeexEchoState* create_echo_state(int frame_size, int filter_length) {
     return speex_echo_state_init(frame_size, filter_length);
 }
 
-void destroy_echo_state(SpeexEchoState* st) {
-    speex_echo_state_destroy(st);
-}
-
-SpeexPreprocessState* create_preprocess_state(int frame_size) {
-    return speex_preprocess_state_init(frame_size, 16000);
-}
-
-void destroy_preprocess_state(SpeexPreprocessState* st) {
-    speex_preprocess_state_destroy(st);
-}
-
-void process_echo(SpeexEchoState* echo_state, short* mic, short* ref, short* output) {
-    speex_echo_cancel(echo_state, mic, ref, output, NULL);
-}
-
-void preprocess(SpeexPreprocessState* preproc, short* input) {
-    speex_preprocess_run(preproc, input);
+void speex_aec_destroy(EchoCanceller *ec){
+	speex_echo_state_destroy(ec->echo_state);
+	free(ec);
 }
 */
 import "C"
@@ -36,36 +35,30 @@ import (
 )
 
 type EchoCanceller struct {
-	echoState  *C.SpeexEchoState
-	preprocess *C.SpeexPreprocessState
-	frameSize  int
+	echoState *C.EchoCanceller
 }
 
-func NewEchoCanceller(frameSize, filterLen int) *EchoCanceller {
-	return &EchoCanceller{
-		echoState:  C.create_echo_state(C.int(frameSize), C.int(filterLen)),
-		preprocess: C.create_preprocess_state(C.int(frameSize)),
-		frameSize:  frameSize,
-	}
+func NewEchoCanceller(frameSize, filterLen int, sample_rate int) *EchoCanceller {
+
+	ec := C.speex_aec_init(C.int(frameSize), C.int(filterLen), C.int(sample_rate))
+	return &EchoCanceller{echoState: ec}
+
 }
 
 func (ec *EchoCanceller) Process(mic, ref []int16) []int16 {
-	output := make([]int16, ec.frameSize)
+	frameSize := len(mic)
+	out := make([]int16, frameSize)
 
-	C.process_echo(ec.echoState,
+	C.speex_aec_process(
+		ec.state,
 		(*C.short)(unsafe.Pointer(&mic[0])),
 		(*C.short)(unsafe.Pointer(&ref[0])),
-		(*C.short)(unsafe.Pointer(&output[0])),
+		(*C.short)(unsafe.Pointer(&out[0])),
+		C.int(frameSize),
 	)
-
-	C.preprocess(ec.preprocess,
-		(*C.short)(unsafe.Pointer(&output[0])),
-	)
-
-	return output
+	return out
 }
 
-func (ec *EchoCanceller) Close() {
-	C.destroy_echo_state(ec.echoState)
-	C.destroy_preprocess_state(ec.preprocess)
+func (ec *EchoCanceller) Destroy() {
+	C.speex_aec_destroy(ec.state)
 }
